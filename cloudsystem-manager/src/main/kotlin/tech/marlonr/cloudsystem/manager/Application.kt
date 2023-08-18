29067e2bed7e4338135d1560cc3d664220664339
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-08-18 20:53:04 +0200

    added ktor server

--- cloudsystem-manager/src/main/kotlin/tech/marlonr/cloudsystem/manager/Application.kt
+++ cloudsystem-manager/src/main/kotlin/tech/marlonr/cloudsystem/manager/Application.kt
@@ -1,5 +1,9 @@
 package tech.marlonr.cloudsystem.manager
 
-fun main() {
+import tech.marlonr.cloudsystem.manager.ktor.KtorService
 
+fun main() {
+    val ktorService = KtorService("0.0.0.0", 8080) //TODO: config file
+
+    ktorService.start()
 }
--- cloudsystem-manager/src/main/kotlin/tech/marlonr/cloudsystem/manager/ktor/KtorService.kt
+++ cloudsystem-manager/src/main/kotlin/tech/marlonr/cloudsystem/manager/ktor/KtorService.kt
@@ -1,7 +1,17 @@
 package tech.marlonr.cloudsystem.manager.ktor
 
-class KtorService {
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kotlin.concurrent.thread
+
+class KtorService(private val host: String, private val port: Int = 8080) {
 
     fun start() {
+        embeddedServer(Netty, host = host, port = port, module = Application::module).start(wait = true)
     }
+}
+
+fun Application.module() {
+
 }

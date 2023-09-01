e3a64fa97b95baebf866dda19a81ab392282dd0a
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-09-01 17:48:21 +0200

    add javadocs for CloudAPI and ICloudGroup

--- cloudsystem-api/src/main/java/tech/marlonr/cloudsystem/api/CloudAPI.java
+++ cloudsystem-api/src/main/java/tech/marlonr/cloudsystem/api/CloudAPI.java
@@ -1,11 +1,13 @@
 package tech.marlonr.cloudsystem.api;
 
-import lombok.Getter;
 import tech.marlonr.cloudsystem.api.group.ICloudGroupProvider;
 import tech.marlonr.cloudsystem.api.instance.ICloudInstanceProvider;
 import tech.marlonr.cloudsystem.api.node.ICloudNodeProvider;
 import tech.marlonr.cloudsystem.api.player.ICloudPlayerProvider;
 
+/**
+ * The base class of the Cloud API
+ */
 public abstract class CloudAPI {
     private static CloudAPI instance;
 
@@ -13,11 +15,29 @@ public abstract class CloudAPI {
         instance = this;
     }
 
+    /**
+     * @return {@link tech.marlonr.cloudsystem.api.group.ICloudGroupProvider}
+     */
     public abstract ICloudGroupProvider getGroupProvider();
+
+    /**
+     * @return {@link tech.marlonr.cloudsystem.api.instance.ICloudInstanceProvider}
+     */
     public abstract ICloudInstanceProvider getInstanceProvider();
+
+    /**
+     * @return {@link tech.marlonr.cloudsystem.api.instance.ICloudInstanceProvider}
+     */
     public abstract ICloudPlayerProvider getCloudPlayerProvider();
+
+    /**
+     * @return {@link tech.marlonr.cloudsystem.api.node.ICloudNodeProvider}
+     */
     public abstract ICloudNodeProvider getCloudNodeProvider();
 
+    /**
+     * @return the instance of {@link tech.marlonr.cloudsystem.api.CloudAPI}
+     */
     public static CloudAPI getInstance() {
         return instance;
     }
--- cloudsystem-api/src/main/java/tech/marlonr/cloudsystem/api/group/ICloudGroup.java
+++ cloudsystem-api/src/main/java/tech/marlonr/cloudsystem/api/group/ICloudGroup.java
@@ -1,6 +1,16 @@
 package tech.marlonr.cloudsystem.api.group;
 
+/**
+ * The Cloud Group that groups Cloud Instances
+ */
 public interface ICloudGroup {
+    /**
+     * @return the name of the group
+     */
     String getName();
+
+    /**
+     * @return The minimum number of instances that must be online.
+     */
     int getMinOnlineCount();
 }

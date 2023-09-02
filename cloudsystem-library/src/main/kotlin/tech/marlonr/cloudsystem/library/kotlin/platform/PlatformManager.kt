4f213960ced39cd2351c39b92dfb05a80683845f
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-09-02 21:19:42 +0200

    added WaterfallPlatform

--- cloudsystem-library/src/main/kotlin/tech/marlonr/cloudsystem/library/kotlin/platform/PlatformManager.kt
+++ cloudsystem-library/src/main/kotlin/tech/marlonr/cloudsystem/library/kotlin/platform/PlatformManager.kt
@@ -4,6 +4,7 @@ import io.ktor.client.*
 import tech.marlonr.cloudsystem.library.kotlin.platform.impl.bukkit.PaperPlatform
 import tech.marlonr.cloudsystem.library.kotlin.platform.impl.bukkit.PurpurPlatform
 import tech.marlonr.cloudsystem.library.kotlin.platform.impl.proxies.VelocityPlatform
+import tech.marlonr.cloudsystem.library.kotlin.platform.impl.proxies.WaterfallPlatform
 
 class PlatformManager(
     private val httpClient: HttpClient = HttpClient { }
@@ -12,6 +13,7 @@ class PlatformManager(
     val platforms = hashMapOf<String, IPlatform>(
         // Proxies
         "Velocity" to VelocityPlatform(httpClient),
+        "Waterfall" to WaterfallPlatform(httpClient),
 
         // Bukkit
         "Paper" to PaperPlatform(httpClient),

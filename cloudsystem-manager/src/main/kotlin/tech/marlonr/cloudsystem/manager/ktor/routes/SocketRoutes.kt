45ddf8ba23a9cd38c8a0d7771e8df439fa9f1136
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-09-05 22:08:25 +0200

    add web socket route

--- cloudsystem-manager/src/main/kotlin/tech/marlonr/cloudsystem/manager/ktor/routes/SocketRoutes.kt
+++ cloudsystem-manager/src/main/kotlin/tech/marlonr/cloudsystem/manager/ktor/routes/SocketRoutes.kt
@@ -1,15 +1,18 @@
 package tech.marlonr.cloudsystem.manager.ktor.routes
 
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
 import tech.marlonr.cloudsystem.api.message.ISocketMessage
 import tech.marlonr.cloudsystem.library.kotlin.message.SocketMessageImpl
 import tech.marlonr.cloudsystem.manager.utils.json
 
 fun Application.configureSocketRoutes() {
     routing {
-        route("/sockets/internal") {
-
+        authenticate {
+            webSocket("/sockets/internal") {
+            }
         }
     }
 }
